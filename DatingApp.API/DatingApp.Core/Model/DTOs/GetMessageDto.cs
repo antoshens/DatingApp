fe3f545108c71a9f666e8bcae5af3cf73629114c
Anton Goncharenko <anthonygoncharenko@gmail.com>
2022-03-20 17:02:28 +0200

    Add GetMessageDto mapping configuration

--- DatingApp.API/DatingApp.Core/Model/DTOs/GetMessageDto.cs
+++ DatingApp.API/DatingApp.Core/Model/DTOs/GetMessageDto.cs
@@ -1,7 +1,17 @@
-﻿namespace DatingApp.Core.Model.DTOs
+﻿using AutoMapper;
+using DatingApp.Core.Model.AutoMapper;
+
+namespace DatingApp.Core.Model.DTOs
 {
-    public class GetMessageDto
+    public class GetMessageDto : IMapTo<Message, GetMessageDto>
     {
+        private readonly IMapper _mapper;
+
+        public GetMessageDto(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
         public int Id { get; set; }
         public string Content { get; set; }
         public int SenderId { get; set; }
@@ -14,5 +24,12 @@
         public DateTime? DateEdited { get; set; }
         public bool IsDeletedForSender { get; set; }
         public bool IsDeletedForRecepient { get; set; }
+
+        public void ConfigureMapTo(IMappingExpression<Message, GetMessageDto> mapping)
+        {
+            mapping.ForMember(x => x.Id, s => s.MapFrom(m => m.MessageId));
+            mapping.ForMember(x => x.Sender, s => s.MapFrom(m => _mapper.Map<UserDto>(m.SenderUser)));
+            mapping.ForMember(x => x.Recepient, s => s.MapFrom(m => _mapper.Map<UserDto>(m.RecipientUser)));
+        }
     }
 }
